942cf8ec467f9d0625e9b79b66196af9c6e536d9
nghianv-1868 <ngo.van.nghia@sun-asterisk.com>
2020-09-24 19:51:40 +0700

    create-contract-bridge-ETH-ONE

---
+++ contracts/migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+// const ConvertLib = artifacts.require('ConvertLib');
+// const MetaCoin = artifacts.require('MetaCoin');
+
+module.exports = function (deployer) {
+  // deployer.deploy(ConvertLib);
+  // deployer.deploy(MetaCoin);
+};
