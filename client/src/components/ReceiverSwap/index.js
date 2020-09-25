a6e6bd316f8afbb903c13a1171794d51e7d3f736
tranchien <chientm@apples-MacBook-Pro.local>
2020-09-25 10:00:35 +0700

    add wallet math + login + logout

---
+++ client/src/components/ReceiverSwap/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Input, Col, Row, Divider } from 'antd';
+import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExchangeAlt } from '@fortawesome/fontawesome-free-solid';
+import './index.css';
+function ReceiverSwap() {
+  const receiverAddress = useSelector(state => state.receiverAddress);
+  return (
+    <div className='receiver-swap'>
+      <Divider orientation='left'>Receiver Address:</Divider>
+      <Row>
+        <Col span={6} offset={1}>
+          <Input
+            className='receiver-address'
+            size='large'
+            disabled={true}
+            value={receiverAddress}
+          />
+        </Col>
+        <Col span={6} offset={6}>
+          <Button size='large' shape='round' className='btn-swap'>
+            <label className='swap-label'>Swap</label>{' '}
+            <FontAwesomeIcon className='swap-icon' icon={faExchangeAlt} />
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ReceiverSwap;
--- client/src/components/WalletPair/index.js
+++ client/src/components/WalletPair/index.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import { Row, Col, Button } from 'antd';
+import { Row, Col } from 'antd';
+import { useSelector } from 'react-redux';
 import MetaMaskWallet from 'components/MetaMaskWallet';
+import MathWallet from 'components/MathWallet';
+import LogoutButton from 'components/LogoutButton';
 import './index.css';
 
 function WalletPair() {
+  const senderAddress = useSelector(state => state.senderAddress);
+  const receiverAddress = useSelector(state => state.receiverAddress);
+
+  let sender;
+  senderAddress
+    ? (sender = (
+        <div>
+          {senderAddress} <LogoutButton isSender={true} />
+        </div>
+      ))
+    : (sender = <MetaMaskWallet isSender={true}></MetaMaskWallet>);
+
+  let receiver;
+  receiverAddress
+    ? (receiver = (
+        <div>
+          {receiverAddress} <LogoutButton isSender={false} />
+        </div>
+      ))
+    : (receiver = <MathWallet isSender={false}></MathWallet>);
+
   return (
     <div className='wallet-pair'>
       <Row gutter={[8, 8]}>
         <Col span={8} className='wallet-left'>
           <div>Ethereum</div>
-          <MetaMaskWallet></MetaMaskWallet>
+          {sender}
         </Col>
 
         <Col span={8} offset={7} className='wallet-right'>
           <div>Harmony</div>
-          <Button type='dashed' size='large'>
-            MathWallet
-          </Button>
+          {receiver}
         </Col>
       </Row>
     </div>
