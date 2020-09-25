95524b9221ffc3c14947c68d5a276fd6c1a0c85a
nghianv-1868 <ngo.van.nghia@sun-asterisk.com>
2020-09-25 18:34:11 +0700

    edit-two-contract-brigde

--- contracts/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// const ConvertLib = artifacts.require('ConvertLib');
-// const MetaCoin = artifacts.require('MetaCoin');
-
-module.exports = function (deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.deploy(MetaCoin);
-};
